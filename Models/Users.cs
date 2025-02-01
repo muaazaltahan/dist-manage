6372666883e424255b83ee2cdfe84926db954aba
muaazaltahan <almuaaz559@gmail.com>
2025-02-01 13:49:23 +0300

    gitignore

---
+++ Models/Users.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace dist_manage.DB
+{
+    public class Users
+    {
+       [Key]
+        public int Id { get; set; } // Primary Key
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Password { get; set; }
+        public int Role { get; set; }
+        public string Notes { get; set; }
+    }
+    public class UsersDB
+    {
+        [Key]
+        public int Id { get; set; } // Primary Key
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Password { get; set; }
+        public int Role { get; set; }
+        public string Notes { get; set; }
+    }
+}
